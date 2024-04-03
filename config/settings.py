50365c573463fddc82a4d72c6e5e6deea779f1a3
Martin <larsen.martin2005@gmail.com>
2024-04-03 18:47:52 +0200

    add allauth with better login and signup

--- config/settings.py
+++ config/settings.py
@@ -36,10 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # crispy apps
     'crispy_forms',
-    'crispy_bootstrap4'
-    ,
+    'crispy_bootstrap4',
+    # allauth apps
+    'allauth',
+    'allauth.account',
 
+    # my apps
     'accounts',
     'pages',
 ]
@@ -52,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -72,6 +79,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+# setting django all auth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
@@ -141,3 +154,15 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 # setting login and logout
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# setting allauth authentication
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_UNIQUE_EMAIL = True
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
